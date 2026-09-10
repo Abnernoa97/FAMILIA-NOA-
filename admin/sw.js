5f17044b0f6c7f100b762bba7dddc9709a7ea752
Abnernoa97 <soloaplicaciones97@gmail.com>
2026-09-10 14:07:46 -0600

    Refresh admin PWA cache

--- admin/sw.js
+++ admin/sw.js
@@ -1,4 +1,4 @@
-const CACHE='familia-noa-admin-v3';
+const CACHE='familia-noa-admin-v4';
 const BASE='/FAMILIA-NOA-/admin/';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.add(BASE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
